9d75c95916e6942a58830bcd45dd9f9f27ab0ae7
Georgy <vgera312@yandex.ru>
2023-05-21 03:16:21 +0400

    main

--- stats/client/src/main/java/ru/practicum/explore/stats/StatsClient.java
+++ stats/client/src/main/java/ru/practicum/explore/stats/StatsClient.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class StatsClient extends BaseClient {
 
     @Autowired
-    public StatsClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("http://stats-server:9090") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
