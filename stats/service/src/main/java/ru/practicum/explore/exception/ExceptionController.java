8ae989131467e5022bd2946bedecef9e92ebb8de
Georgy <vgera312@yandex.ru>
2023-05-27 15:55:21 +0400

    main

--- stats/service/src/main/java/ru/practicum/explore/exception/ExceptionController.java
+++ stats/service/src/main/java/ru/practicum/explore/exception/ExceptionController.java
@@ -2,10 +2,13 @@ package ru.practicum.explore.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -24,5 +27,26 @@ public class ExceptionController {
     public ApiError handleIncorrectValidationException(ValidationException e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
         return new ApiError(e.getMessage(), "The required object was not found.", HttpStatus.BAD_REQUEST, List.of());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiError methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return new ApiError(e.getMessage(), "An unexpected error has occurred.", HttpStatus.BAD_REQUEST, List.of());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ApiError missingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return new ApiError(e.getMessage(), "An unexpected error has occurred.", HttpStatus.BAD_REQUEST, List.of());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ApiError methodPositiveOrZeroValid(ConstraintViolationException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return new ApiError(e.getMessage(), "An unexpected error has occurred.", HttpStatus.BAD_REQUEST, List.of());
     }
 }
